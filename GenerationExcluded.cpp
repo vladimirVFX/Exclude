2bae9b71d48be43bc7cb5c9a285234f4a3b0516a
VladimirMironov <44780782+vladimirVFX@users.noreply.github.com>
2020-04-19 17:40:10 +0300

    Add files via upload

---
+++ GenerationExcluded.cpp
@@ -0,0 +1,39 @@
+﻿// GenerationExcluded.cpp 
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > nul");
+
+    cout << "Solve Ax = B" << "\n";
+    double A, B, C;
+    
+    cout << "A = ";
+    cin >> A;
+    cout << "B = ";
+    cin >> B;
+
+    try {
+        if (A != 0)
+            throw A; 
+        if (B != 0)
+            throw "No";
+        cout << "Any solve";
+    }
+
+    catch (double e) {
+        cout << "Solve: " << B / e << endl;
+    }
+
+    catch (char* e) {
+        cout << e << endl;
+    }
+
+    system("pause > nul");
+    return 0;
+}
+
